cf2515bbd2f7a32ba16b3df20b35fd47772a0421
leduy <3hinney@gmail.com>
2024-01-30 17:23:37 +0700

    new project

--- README.md
+++
@@ -1,1 +0,0 @@
-# rtyque.github.io
--- scripts.js
+++ scripts.js
@@ -1,71 +1,37 @@
-var Engine = Matter.Engine,
-    Render = Matter.Render,
-    Runner = Matter.Runner,
-    Bodies = Matter.Bodies,
-    Body = Matter.Body,
-    Constraint = Matter.Constraint,
-    MouseConstraint = Matter.MouseConstraint,
-    Mouse = Matter.Mouse,
-    Events = Matter.Events,
-    Composite = Matter.Composite;
+function randomPos(min, max){
+    return Math.floor(Math.random() * (max - min) + min)
+}
 
-function onLoad(){
-    // create an engine
-    var engine = Engine.create();
 
-    // create a renderer
-    var render = Render.create({
-        element: document.body,
-        engine: engine,
-        options: {
-            background: '#fafafa',
-            wireframes: false
-        }
-    });
-
-    // create two boxes and a ground
-    let ball = Bodies.circle(200,400,50, {density:0.004, render:{
-        sprite: {texture: 'ubhs.png'}
-    }});
-    let elastic = Constraint.create({
-        pointA: {x:200,y:400},
-        bodyB: ball,
-        stiffness: 0.05
-    })
-    var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-    var can1 = Bodies.rectangle(600, 575, 90, 10, { isStatic: true });
-    var can2 = Bodies.rectangle(545, 535, 90, 10, { isStatic: true });
-    Body.rotate(can2, Math.PI/2.5);
-    var can3 = Bodies.rectangle(655, 535, 90, 10, { isStatic: true });
-    Body.rotate(can3, -Math.PI/2.5);
-
-    let mouse=Mouse.create(render.canvas);
-    let mouseConstraint = MouseConstraint.create(engine, {
-        mouse: mouse
-    })
-
-    function NewBall() {
-        ball = Bodies.circle(200,400,50, {density:0.004, render:{
-            sprite: {texture: 'ubhs.png'}
-        }})
-        Composite.add(engine.world, ball);
-        elastic.bodyB = ball;
+function updatePos(mousePos, button_pos){
+    var no_button=document.getElementById('no');
+    if (button_pos.x<window.innerWidth/2-50){
+        newX=button_pos.x+randomPos(1,window.innerWidth-button_pos.right)+100;
     }
-    Events.on(engine, 'afterUpdate', function (){
-        if (mouseConstraint.mouse.button === -1 && (ball.position.x > 220 || ball.position.y < 380)) {
-            NewBall();
+    else{
+        newX=button_pos.x-randomPos(1,button_pos.left);
+    }
+    if (button_pos.y<window.innerHeight/2-35){
+        newY=button_pos.y+randomPos(50,window.innerHeight-button_pos.bottom)+70;
+    }
+    else{
+        newY=button_pos.y-randomPos(1,button_pos.top);
+    }
+    
+    no_button.style.left=newX+'px';
+    no_button.style.top=newY+'px';
+}
+
+function main(){
+    
+    var no_button=document.getElementById('no');
+    button_pos=no_button.getBoundingClientRect();
+    window.addEventListener('mousemove', (event) => {
+        mousePos = { x: event.clientX, y: event.clientY };
+        if (mousePos.x>button_pos.x && mousePos.x<button_pos.x+100 && mousePos.y>button_pos.y && mousePos.y<button_pos.y+70){
+            updatePos(mousePos, button_pos);
+            button_pos=no_button.getBoundingClientRect();
         }
-    });
-    // add all of the bodies to the world
-    Composite.add(engine.world, [ball, ground, elastic, mouseConstraint, can1,can2,can3]);
-
-    // run the renderer
-    Render.run(render);
-
-    // create runner
-    var runner = Runner.create();
-
-    // run the engine
-    Runner.run(runner, engine);
-    render.mouse=mouse;
+        
+    })
 }
